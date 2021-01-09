cf5c5cb143d50a88c19391ab4a645055f52afa3a
Anthony Sim <anthonyysim@gmail.com>
2021-01-09 13:15:59 -0800

    Adv Content - refactored index.js and counter.js to make use of promises

--- datastore/counter.js
+++ datastore/counter.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const sprintf = require('sprintf-js').sprintf;
-
+var Promise = require('bluebird');
+const pfs = Promise.promisifyAll(require('fs'));
 
 var counter = 0;
 
@@ -16,25 +17,39 @@ const zeroPaddedNumber = (num) => {
   return sprintf('%05d', num);
 };
 
-const readCounter = (callback) => { // (err, id) => writeCounter(id + 1, callback)
-  fs.readFile(exports.counterFile, (err, fileData) => {
-    if (err) {
-      callback(null, 0); // (err, id) => writeCounter(id + 1, callback)
-    } else {
-      callback(null, Number(fileData)); // (err, id) => writeCounter(id + 1, callback)
-    }
-  });
+// const readCounter = (callback) => { // (err, id) => writeCounter(id + 1, callback)
+//   fs.readFile(exports.counterFile, (err, fileData) => {
+//     if (err) {
+//       callback(null, 0); // (err, id) => writeCounter(id + 1, callback)
+//     } else {
+//       callback(null, Number(fileData)); // (err, id) => writeCounter(id + 1, callback)
+//     }
+//   });
+// };
+
+const readCounter = (callback) => {
+  pfs.readFileAsync(exports.counterFile)
+    .then((data) => callback(null, Number(data)))
+    .catch(() => callback(null, 0));
 };
 
-const writeCounter = (count, callback) => { // exports.create = (text, callback)
+// const writeCounter = (count, callback) => { // exports.create = (text, callback)
+//   var counterString = zeroPaddedNumber(count);
+//   fs.writeFile(exports.counterFile, counterString, (err) => {
+//     if (err) {
+//       throw ('error writing counter');
+//     } else {
+//       callback(null, counterString); // exports.create = (text, callback)
+//     }
+//   });
+// };
+
+const writeCounter = (count, callback) => {
   var counterString = zeroPaddedNumber(count);
-  fs.writeFile(exports.counterFile, counterString, (err) => {
-    if (err) {
-      throw ('error writing counter');
-    } else {
-      callback(null, counterString); // exports.create = (text, callback)
-    }
-  });
+
+  pfs.writeFileAsync(exports.counterFile, counterString)
+    .then(() => callback(null, counterString))
+    .catch(() => console.error('error writing counter'));
 };
 
 // Public API - Fix this function //////////////////////////////////////////////
